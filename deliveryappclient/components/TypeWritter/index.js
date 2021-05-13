46c39c3c221a0d535a28b77c7381a78bea6b0ca5
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-05-13 11:06:17 -0700

    home page animation refactor

--- deliveryappclient/components/TypeWritter/index.js
+++ deliveryappclient/components/TypeWritter/index.js
@@ -1,23 +1,23 @@
 import { useState, useEffect } from 'react';
 
-export default function TypeWritter(props) {
+export default function Typewriter(props) {
     const [phrase, setPhrase] = useState('');
 
     useEffect(() => {
         let currentText = '';
         props.text.split('').forEach((char, index) => {
-            setTimeout(() => {
-                currentText = currentText.slice(0, -1);
-                console.log(currentText)
-                currentText += char;
-                if(props.text.lenght != index +1)
-                    currentText += '❙'
-                setPhrase(currentText);
-            }, 200 + (index * 100));
+        setTimeout( () => {
+            currentText = currentText.slice(0, -1);
+            currentText += char;
+            if(props.text.length != index + 1)
+            currentText += "❙"
+            setPhrase(currentText);
+        }, 200 + (index * 100));
         })
     }, [])
 
-    return (
+
+    return(
         <>{phrase}</>
     )
 }
