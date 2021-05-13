c12071308f6ab225e19018674ae8d8d1ad6d1295
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-05-12 18:47:42 -0700

    theme colors/ background image

--- deliveryappclient/pages/index.js
+++ deliveryappclient/pages/index.js
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Container from 'react-bootstrap/Container';
+
 
 export default function Home() {
   return (
-    <>
-    </>
+    <Container>
+      Home Page =)
+    </Container>
   )
 }
