b690ec89e1879e42306d3ae3d0e1a8a8c0d5f798
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-05-19 16:48:21 -0700

    refactored get restaurants service

--- deliveryappclient/services/getRestaurants.js
+++ deliveryappclient/services/getRestaurants.js
@@ -7,22 +7,22 @@ export default function getRestaurants() {
     const router = useRouter();
     const { category, q } = router.query;
     const [address] = useRecoilState(addressState)
-
-    let params = '';
+  
+    let params = [];
     if(category)
-        params = `${params == '' ? '?' : '&'}category=${category}`
+      params.push(`${params == '' ? '?' : '&'}category=${category}`);
     if(q)
-        params = `${params == '' ? '?' : '&'}q=${q}`
-    if(address.city != '')
-        params = `${params == '' ? '?' : '&'}city=${address.city}`
-
+      params.push(`${params == '' ? '?' : '&'}q=${q}`);
+    if(address.city !== '')
+      params.push(`${params == '' ? '?' : '&'}city=${encodeURIComponent(address.city)}`);
+  
     const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
+    const url = `${process.env.apiUrl}/api/restaurants${params.join('')}`;
     const { data, error } = useSWR(
-        `${process.env.apiUrl}/api/restaurants${params}`,
-        fetcher, 
-            { revalidateOnFocus: false }
-        )
-
+      url,
+      fetcher, 
+      { revalidateOnFocus: false }
+    )
+  
     return { restaurants: data, isLoading: !error && !data, isError: error }
-}
+  }
