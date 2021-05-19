59c8549b857e397b41d6fb6e0e088acb812f5581
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-05-19 16:49:49 -0700

    added product modal to Category Products

--- deliveryappclient/components/RestaurantDetails/CategoryProducts/index.js
+++ deliveryappclient/components/RestaurantDetails/CategoryProducts/index.js
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import { Row, Col, Card } from 'react-bootstrap';
 import Image from 'next/image'
 import toCurrency from '../../../services/toCurrency';
 import truncateString from '../../../services/truncateString';
+import AddProductModal from '../../AddProductModal';
 
 export default function CategoryProducts(props) {
+    const [selectedProduct, setSelectedProduct] = useState(null);
 
 return(
     <>
         <h5 className='fw-bold'>{props.title}</h5>
         <Row>
             {props.products.map((product, i) =>
-                <Col md={4} sm={12} key={i}>
-                    <Card className="mb-4 clickable_effect">
+                <Col md={6} sm={12} key={i}>
+                    <Card className='mb-4 clickable_effect' onClick={() => setSelectedProduct(product)}>
                         <Row className="my-3 mx-1">
                             <Col md={6} xs={{span: 12, order: 2 }}>
                                 <p className='fw-bold mb-0'>{product.name}</p>
@@ -35,6 +38,12 @@ return(
                 </Col>
             )}
         </Row>
+        <AddProductModal
+                show={selectedProduct != null}
+                onHide={() => setSelectedProduct(null)}
+                product={selectedProduct}
+                restaurant={props.restaurant}
+        />
     </>
 )
 }
