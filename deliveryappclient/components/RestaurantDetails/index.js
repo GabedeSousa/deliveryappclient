bb78911b8d08bda070ed3037716fba4ac9a65c9e
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-05-21 15:13:20 -0700

    restaurant details props

--- deliveryappclient/components/RestaurantDetails/index.js
+++ deliveryappclient/components/RestaurantDetails/index.js
@@ -1,24 +1,13 @@
 import Details from './Details';
 import CategoryProducts from './CategoryProducts';
-import getRestaurant from '../../services/getRestaurant';
-import { useRouter } from 'next/router';
-import { Spinner,  Alert } from 'react-bootstrap';
 
-export default function RestaurantDetails() {
-    const router = useRouter();
-    const { id } = router.query;
+export default function RestaurantDetails(props) {
 
-    const { restaurant, isLoading, isError } = getRestaurant(id);
-
-    if(isError)
-        return <Alert variant='custom-red'>Erro ao carregar.</Alert>
-    else if(isLoading)
-        return <Spinner animation='border' />
     return (
         <>
-            <Details {...restaurant}/>
-            {restaurant.product_categories.map((product_category, i) => 
-                <CategoryProducts restaurant={restaurant} {...product_category} key={i} />
+            <Details {...props.restaurant}/>
+            {props.restaurant.product_categories.map((product_category, i) => 
+                <CategoryProducts restaurant={props.restaurant} {...product_category} key={i} />
             )}
         </>
     )
